af6fba2eaf9cd7049c35b0ee60451edcd4e0be21
rezahakimalzami03 <rezahakimalzami03@gmail.com>
2024-04-19 16:59:02 +0700

    Ubah teks Pelaporan

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -35,7 +35,7 @@ const Homepage = () => {
                             </svg>
                         </a>
                         <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
-                            Learn more
+                            Pelaporan
                         </a>
                     </div>
                 </div>
